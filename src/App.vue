3dae210cba0d34df545a9781fd8544f44815fc93
龚少瑜 <gongshaoyu@xindong.com>
2018-12-11 12:33:22 +0800

    cookie包，配置文件规范化，语言包自定义加载，页面语言包测试

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <code v-text="'<el-button>'"></code>
         below
       </p>
-      <el-button>el-button</el-button>
+      <el-button >{{ $t('home.test') }}</el-button>
     </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from "vue";
+import Element from "element-ui";
+import "./assets/css/element-variables.scss";
+import i18n from "./lang/i18n";
+import router from "./config/router";
+import store from "./config/store";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./plugins/element.js";
+
+Vue.use(Element, {
+  i18n: (key, value) => i18n.t(key, value)
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
